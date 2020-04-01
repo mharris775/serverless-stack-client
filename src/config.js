e6d1c0972f8d76f6f309c462f3fc3bff541fa300
mharris775 <61429444+mharris775@users.noreply.github.com>
2020-04-01 12:49:32 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "elbert-ballo-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://hbzgm5jveh.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_7vp3PZ8LD",
+    APP_CLIENT_ID: "210575m5oo96c6d9d7h819c0no",
+    IDENTITY_POOL_ID: "us-east-1:8e769d38-64dc-4188-9523-34f91137a473"
+  }
+};
