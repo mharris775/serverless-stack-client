0397fbccd538d760bfc3f5598fae141382528c27
mharris775 <61429444+mharris775@users.noreply.github.com>
2020-04-02 15:01:41 -0700

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_ZFLVAbyg0aXc1fOdPx16bWld00la9RXhj0",
   s3: {
     REGION: "us-east-1",
     BUCKET: "elbert-ballo-notes-app-uploads"
